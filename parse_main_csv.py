02faad4613f083b4db2498fd0b71128f2157897d
ramagreddy <ramagurijala@yahoo.com>
2024-10-18 13:13:44 +0100

    Update parse_main_csv.py

--- parse_main_csv.py
+++ parse_main_csv.py
@@ -10,9 +10,11 @@ wrapup_codes = {}
 with open(csv_file, mode='r') as file:
     csv_reader = csv.DictReader(file)
     for index, row in enumerate(csv_reader):
-        wrapup_codes[f"wrapup_code_{index}"] = json.dumps({
-            "name": row['name']
-        })
+        wrapup_codes[f"wrapup_code_{row['name']}"] = json.dumps({
+            "name": row['name'],
+            "rename": row['rename'],
+            "division": row['division'] if row['division'] else "All",
+        })        
 
 # Output the JSON data as a map of string keys and string values
 print(json.dumps(wrapup_codes))
